b9f8d72c2c5163c03c83d74570b2d68126253fdf
ilanherbach <ilan.herbach@gmail.com>
2019-09-15 23:41:25 -0300

    Add two numbers and display the sum

---
+++ 01_sequencial_structure/03_add_two_numbers.py
@@ -0,0 +1,10 @@
+# Asks the user 2 numbers than prints the sum
+
+print("Lets add 2 numbers :)")
+
+num1 = int(input("Type the first number: "))
+num2 = int(input("Type the second number: "))
+
+sum = num1 + num2
+
+print("The sum of {} with {} is {}.".format(num1, num2, sum))
