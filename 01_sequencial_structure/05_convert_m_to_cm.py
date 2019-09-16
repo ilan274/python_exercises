89e7331e1213258de8f79f970c3aa83c71630811
ilanherbach <ilan.herbach@gmail.com>
2019-09-15 23:42:21 -0300

    Convert meters to centimeters

---
+++ 01_sequencial_structure/05_convert_m_to_cm.py
@@ -0,0 +1,7 @@
+# Create a program that converts meters to centimeters
+
+meters = float(input("How many meters would you like to convert to centimeters? "))
+
+calc = meters * 100
+
+print("{} meters in centimeters is {:.2f}.".format(meters, calc))
