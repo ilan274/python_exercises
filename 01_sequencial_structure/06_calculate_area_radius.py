f8c2811affc4a1cc65824c7584603a8857d81ce4
ilanherbach <ilan.herbach@gmail.com>
2019-09-15 23:43:15 -0300

    Asks for radius than calculate and show its area

---
+++ 01_sequencial_structure/06_calculate_area_radius.py
@@ -0,0 +1,7 @@
+# Make a program that asks for the radius of a circle than calculates and shows its area
+
+pi = 3.14159265359
+radius = float(input("Type the area of the circle in meters: "))
+area = pi * (radius**2)
+
+print("The area of the circle is {:.2f}.".format(area))
